2a7bd03a0d77ee117e214fc7585275e0752d48b6
Jonathan Stubbs <Jonathan Stubbs@NOVATECH06>
2017-02-08 11:43:05 +0000

    Add project files.

---
+++ testAssociation/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testAssociation
+{
+    public partial class Form1 : Form
+    {
+        public List<Address> addressBook;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addressBook = new List<Address>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addressBook.Add(new Address());
+
+            
+            myHouse.residents.Add(new Person("Peter"));
+
+            foreach (var house in )
+            {
+                MessageBox.Show (house.)
+            }
+            
+        }
+    }
+}
